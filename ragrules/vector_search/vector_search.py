1e8ad63960022738c1577be3e3c8b192c04bffd0
Thibault <thibault.romann@gmail.com>
2024-10-22 19:32:01 +0200

    object types

--- ragrules/vector_search/vector_search.py
+++ ragrules/vector_search/vector_search.py
@@ -2,18 +2,18 @@ from google.cloud.aiplatform_v1 import FindNeighborsRequest, FindNeighborsRespon
 
 
 class VectorSearchClient:
-    def __init__(self, api_endpoint, index_endpoint, deployed_index_id) -> None:
+    def __init__(self, api_endpoint: str, index_endpoint: str, deployed_index_id: str) -> None:
         self.api_endpoint = api_endpoint
         self.index_endpoint = index_endpoint
         self.deployed_index_id = deployed_index_id
         self.client = MatchServiceClient(client_options={"api_endpoint": self.api_endpoint})
 
-    def build_query(self, feature_vector, neighbor_count=10) -> FindNeighborsRequest.Query:
+    def build_query(self, feature_vector: list, neighbor_count=10) -> FindNeighborsRequest.Query:
         datapoint = IndexDatapoint(feature_vector=feature_vector)
         query = FindNeighborsRequest.Query(datapoint=datapoint, neighbor_count=neighbor_count)
         return query
 
-    def find_neighbors(self, feature_vector, neighbor_count=10, return_full_datapoint=False) -> FindNeighborsResponse:
+    def find_neighbors(self, feature_vector: list, neighbor_count=10, return_full_datapoint=False) -> FindNeighborsResponse:
         query = self.build_query(feature_vector, neighbor_count)
         request = FindNeighborsRequest(
             index_endpoint=self.index_endpoint,
